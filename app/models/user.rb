82cc8cd0799e9b15a5351de88a00e61ea8c3c340
Braxton Eddings <beddings81@gmail.com>
2023-04-12 01:02:34 -0500

    Set up devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :recoverable, :validatable, :confirmable, :omniauthable
+  devise :database_authenticatable, :recoverable, :validatable, :confirmable, :omniauthable, :registerable
 
   enum role: [:user, :admin]
 end
